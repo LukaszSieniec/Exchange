03f40e948fae9c025a50d482547cb943551482b1
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-06 14:41:40 +0200

    Improve main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,35 @@
+import 'package:exchange/common/app_environment.dart';
+import 'package:exchange/common/dependency_injection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
+void initializeRunConfiguration({required AppEnvironment environment}) async {
+  /// Ensure to run native code before the application starts.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Allow to use the application in portrait mode only.
+  await SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  );
+
+  /// Configure dependency injection.
+  configureDependencies(environment);
+
+  runApplication();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void runApplication() => runApp(const AppInitializationWidget());
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class AppInitializationWidget extends StatelessWidget {
+  const AppInitializationWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp.router(
+        title: 'Exchange',
+        debugShowCheckedModeBanner: false,
+        backButtonDispatcher: RootBackButtonDispatcher(),
+      );
 }
---
+++ lib/main_dev.dart
@@ -0,0 +1,4 @@
+import 'package:exchange/common/app_environment.dart';
+import 'package:exchange/main.dart';
+
+void main() => initializeRunConfiguration(environment: develop);
