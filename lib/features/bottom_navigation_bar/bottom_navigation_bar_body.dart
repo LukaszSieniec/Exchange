d1d57c7cd85c9cbfd9a2ff01f3dfb29349e8b232
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-07 00:10:05 +0200

    Add basic bottom navigation bar

---
+++ lib/features/bottom_navigation_bar/bottom_navigation_bar_body.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarBody extends StatelessWidget {
+  const BottomNavigationBarBody({super.key});
+
+  @override
+  Widget build(BuildContext context) => const SizedBox();
+}
---
+++ lib/features/bottom_navigation_bar/bottom_navigation_bar_page.dart
@@ -0,0 +1,9 @@
+import 'package:exchange/features/bottom_navigation_bar/bottom_navigation_bar_body.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarPage extends StatelessWidget {
+  const BottomNavigationBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) => const BottomNavigationBarBody();
+}
