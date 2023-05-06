2e876f5e736349f6c5f9815ef34797908ebd9964
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-06 13:32:18 +0200

    Implement app router

--- lib/features/splash/presentation/splash_page.dart
+++ lib/features/splash/presentation/splash_page.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:exchange/features/splash/presentation/splash_body.dart';
 import 'package:flutter/material.dart';
 
+@RoutePage()
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
