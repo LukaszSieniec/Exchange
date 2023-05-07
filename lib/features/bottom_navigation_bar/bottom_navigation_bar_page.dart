9d8f46e08ff9b7aa39fec338f5a671ab08c4b822
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-07 02:03:08 +0200

    Add next version of bottom navigation bar

--- lib/features/bottom_navigation_bar/bottom_navigation_bar_page.dart
+++ lib/features/bottom_navigation_bar/bottom_navigation_bar_page.dart
@@ -1,11 +1,17 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:exchange/common/dependency_injection.dart';
 import 'package:exchange/features/bottom_navigation_bar/bottom_navigation_bar_body.dart';
+import 'package:exchange/features/bottom_navigation_bar/cubit/bottom_navigation_bar_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class BottomNavigationBarPage extends StatelessWidget {
   const BottomNavigationBarPage({super.key});
 
   @override
-  Widget build(BuildContext context) => const BottomNavigationBarBody();
+  Widget build(BuildContext context) => BlocProvider(
+        create: (context) => inject<BottomNavigationBarCubit>(),
+        child: BottomNavigationBarBody(),
+      );
 }
