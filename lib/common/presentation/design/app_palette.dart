7a7931ddc0fe5f02ace478bb88ba57c05508ff02
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-10 12:46:57 +0200

    Add colors

--- lib/common/presentation/design/app_palette.dart
+++ lib/common/presentation/design/app_palette.dart
@@ -3,11 +3,19 @@ import 'package:flutter/material.dart';
 /// Holds a palette of colors used for final theming.
 class AppPalette {
   /// Background
-  static const Color backgroundDefault = Color(0xFFFFFFFF);
+  static const Color backgroundDefault = Color(0xFF2E2E47);
+  static const Color backgroundBright = Color(0xFF34344f);
 
   /// Text
   static const Color textDefault = Color(0xFFFFFFFF);
+  static const Color textWarning = Color(0xFFDF441E);
+  static const Color textSuccess = Color(0xFF39937E);
 
   /// Button
-  static const Color buttonDefault = Color(0xFFFFFFFF);
+  static const Color buttonDefault = Color(0xFF47B89E);
+
+  /// Reusable
+  static const Color grayDark = Color(0xFF2B3336);
+  static const Color gray = Color(0xFF546469);
+  static const Color grayBright = Color(0xFF96A7AB);
 }
