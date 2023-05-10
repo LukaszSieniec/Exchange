6e0f2e39e77f0afe0bf4a1394cdd69e2e0f40b53
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-11 00:28:45 +0200

    Upgrade flutter and dependencies

--- lib/features/splash/presentation/widget/splash_label.dart
+++ lib/features/splash/presentation/widget/splash_label.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 
 /// Displays the label on the [SplashBody].
 class SplashLabel extends StatelessWidget {
-  static const label = 'D i N E R O';
+  static const _label = 'D i N E R O';
 
   const SplashLabel({super.key});
 
   @override
   Widget build(BuildContext context) => Center(
         child: Text(
-          label,
+          _label,
           style: Theme.of(context).textTheme.displayLarge!.copyWith(
                 fontWeight: FontWeight.bold,
                 fontStyle: FontStyle.italic,
