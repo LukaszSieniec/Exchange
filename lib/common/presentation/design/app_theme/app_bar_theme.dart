694075f09059a589fb015ed85052183299b2afe7
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-10 12:49:50 +0200

    Add app bar theme

---
+++ lib/common/presentation/design/app_theme/app_bar_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+AppBarTheme get appBarTheme => _buildAppBarTheme();
+
+AppBarTheme _buildAppBarTheme() => const AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+    );
