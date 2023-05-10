8703fffe180246cfd277e59f0f42e35105eb2638
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-10 12:59:48 +0200

    Add outlined button theme

--- lib/common/presentation/design/app_palette.dart
+++ lib/common/presentation/design/app_palette.dart
@@ -2,20 +2,25 @@ import 'package:flutter/material.dart';
 
 /// Holds a palette of colors used for final theming.
 class AppPalette {
-  /// Background
+  /// Background.
   static const Color backgroundDefault = Color(0xFF2E2E47);
   static const Color backgroundBright = Color(0xFF34344f);
 
-  /// Text
+  /// Text.
   static const Color textDefault = Color(0xFFFFFFFF);
-  static const Color textWarning = Color(0xFFDF441E);
-  static const Color textSuccess = Color(0xFF39937E);
+  static const Color textGreen = Color(0xFF39937E);
+  static const Color textRed = Color(0xFFDF441E);
 
-  /// Button
+  /// Button.
   static const Color buttonDefault = Color(0xFF47B89E);
+  static const Color buttonDisabled = Color(0xFFBCC8CC);
 
-  /// Reusable
+  /// Border
+  static const Color borderWhite = Color(0xFFE4EAEB);
+  static const Color borderGreen = Color(0xFF47B89E);
+
+  /// Reusable colors.
   static const Color grayDark = Color(0xFF2B3336);
-  static const Color gray = Color(0xFF546469);
+  static const Color grayMedium = Color(0xFF546469);
   static const Color grayBright = Color(0xFF96A7AB);
 }
---
+++ lib/common/presentation/design/app_theme/outlined_button_theme.dart
@@ -0,0 +1,25 @@
+import 'package:exchange/common/presentation/design/app_palette.dart';
+import 'package:exchange/common/presentation/design/app_theme/text_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+OutlinedButtonThemeData get outlinedButtonTheme => _buildOutlinedButtonTheme();
+
+OutlinedButtonThemeData _buildOutlinedButtonTheme() => OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        backgroundColor: AppPalette.backgroundDefault,
+        textStyle: TextStyle(
+          color: AppPalette.textGreen,
+          fontSize: 16.0.sp,
+          fontWeight: FontWeight.w500,
+          fontFamily: appFontFamily,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+        side: const BorderSide(
+          color: AppPalette.borderGreen,
+          width: 1.5,
+        ),
+      ),
+    );
