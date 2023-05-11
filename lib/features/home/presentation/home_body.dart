7673b8a4c2f9d50b0e6f781095ac1b47f1944739
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-12 01:53:05 +0200

    Improvements UI

--- lib/features/home/presentation/home_body.dart
+++ lib/features/home/presentation/home_body.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:exchange/common/presentation/widgets/app_header.dart';
+import 'package:exchange/features/home/presentation/widget/cryptocurrency_trend_list.dart';
 import 'package:exchange/generated/locale_keys.g.dart';
 import 'package:flutter/material.dart';
 
@@ -8,10 +9,12 @@ class HomeBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AppHeader(
             label: LocaleKeys.common_home.tr(),
           ),
+          const CryptocurrencyTrendList(),
         ],
       );
 }
