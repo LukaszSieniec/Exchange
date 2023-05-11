0ae2f84b6ccd0ae108964a08c136b8b597e25d22
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-12 01:47:21 +0200

    Add locale keys in home body

--- lib/features/home/presentation/home_body.dart
+++ lib/features/home/presentation/home_body.dart
@@ -1,14 +1,16 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:exchange/common/presentation/widgets/app_header.dart';
+import 'package:exchange/generated/locale_keys.g.dart';
 import 'package:flutter/material.dart';
 
 class HomeBody extends StatelessWidget {
   const HomeBody({super.key});
 
   @override
-  Widget build(BuildContext context) => const Column(
+  Widget build(BuildContext context) => Column(
         children: [
           AppHeader(
-            label: 'Home',
+            label: LocaleKeys.common_home.tr(),
           ),
         ],
       );
