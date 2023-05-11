5956e16b51356d627191e355ebd02bb0cde3613c
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-12 01:46:05 +0200

    Add file for cryptocurrency trend list

---
+++ lib/features/home/presentation/widget/cryptocurrency_trend_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CryptocurrencyTrendList extends StatelessWidget {
+  const CryptocurrencyTrendList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class CryptocurrencyTrendTile extends StatelessWidget {
+  const CryptocurrencyTrendTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
