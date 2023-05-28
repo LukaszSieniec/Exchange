f3df7ef565270dd73544c64a185aa8baae27d159
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-05-29 01:57:36 +0200

    Add app rounded network image

---
+++ lib/common/presentation/widgets/app_rounded_network_image.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppRoundedNetworkImage extends StatelessWidget {
+  final String imageUrl;
+
+  const AppRoundedNetworkImage({
+    required this.imageUrl,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => CircleAvatar(
+        radius: 24.0.r,
+        backgroundImage: NetworkImage(imageUrl),
+        backgroundColor: Colors.transparent,
+      );
+}
