cbbba33eb490b183c10577d9f55adb6895c8857d
Łukasz Sieniec <lukasz.sieniec@itcraft.pl>
2023-06-03 14:28:50 +0200

    Implement methods in repository

--- lib/core/cryptocurrencies/data/repository/coin_gecko_cryptocurrencies_repository.dart
+++ lib/core/cryptocurrencies/data/repository/coin_gecko_cryptocurrencies_repository.dart
@@ -1,57 +1,99 @@
+import 'package:dio/dio.dart';
 import 'package:exchange/common/result.dart';
 import 'package:exchange/core/cryptocurrencies/data/network/cryptocurrencies_api.dart';
+import 'package:exchange/core/cryptocurrencies/data/network/dto/cryptocurrency_market/cryptocurrency_market_response.dart';
+import 'package:exchange/core/cryptocurrencies/data/network/dto/market_chart_response.dart';
 import 'package:exchange/core/cryptocurrencies/domain/entity/cryptocurrency_market_entity.dart';
 import 'package:exchange/core/cryptocurrencies/domain/entity/market_chart_entity.dart';
 import 'package:exchange/core/cryptocurrencies/domain/repository/cryptocurrencies_repository.dart';
+import 'package:rxdart/rxdart.dart';
 
 class CoinGeckoCryptocurrenciesRepository
     implements CryptocurrenciesRepository {
   final CryptocurrenciesApi _cryptocurrenciesApi;
 
-  const CoinGeckoCryptocurrenciesRepository(this._cryptocurrenciesApi);
+  final BehaviorSubject<List<CryptocurrencyMarketEntity>> cryptocurrencies;
+
+  CoinGeckoCryptocurrenciesRepository(this._cryptocurrenciesApi)
+      : cryptocurrencies = BehaviorSubject();
 
   @override
-  Future<Result<List<CryptocurrencyMarketEntity>, Exception>>
-      fetchCryptocurrenciesMarket(
-          {required bool clearCryptocurrencies,
-          required String vsCurrency,
-          required String order,
-          required int pageSize,
-          required int pageIndex,
-          required bool sparkline,
-          required String priceChangePercentage}) {
-    // TODO: implement fetchCryptocurrenciesMarket
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Result<List<CryptocurrencyMarketEntity>, Exception>>
-      fetchCryptocurrenciesMarketByIds(
-          {required String vsCurrency,
-          required String ids,
-          required String order,
-          required bool sparkline,
-          required String priceChangePercentage}) {
-    // TODO: implement fetchCryptocurrenciesMarketByIds
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Result<Object, Exception>> fetchCryptocurrencyById(
-      {required String id, required bool marketData, required bool sparkline}) {
-    // TODO: implement fetchCryptocurrencyById
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Result<MarketChartEntity, Exception>> fetchMarketChart(
-      {required String id, required String vsCurrency, required String days}) {
-    // TODO: implement fetchMarketChart
-    throw UnimplementedError();
-  }
-
-  @override
-  // TODO: implement observeCryptocurrenciesMarket
   Stream<List<CryptocurrencyMarketEntity>> get observeCryptocurrenciesMarket =>
-      throw UnimplementedError();
+      cryptocurrencies.stream;
+
+  @override
+  Future<Result<List<CryptocurrencyMarketEntity>, Exception>>
+      fetchCryptocurrenciesMarket({
+    required bool clearCryptocurrencies,
+    required String vsCurrency,
+    required String order,
+    required int pageSize,
+    required int pageIndex,
+    required bool sparkline,
+    required String priceChangePercentage,
+  }) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<List<CryptocurrencyMarketEntity>, Exception>>
+      fetchCryptocurrenciesMarketByIds({
+    required String vsCurrency,
+    required String ids,
+    required String order,
+    required bool sparkline,
+    required String priceChangePercentage,
+  }) async {
+    try {
+      final List<CryptocurrencyMarketResponse> cryptocurrenciesResponse =
+          await _cryptocurrenciesApi.fetchCryptocurrenciesMarketByIds(
+        vsCurrency,
+        ids,
+        order,
+        sparkline,
+        priceChangePercentage,
+      );
+
+      final List<CryptocurrencyMarketEntity> cryptocurrencies =
+          cryptocurrenciesResponse
+              .map((currencyResponse) => currencyResponse.toEntity())
+              .toList();
+
+      return Result.success(data: cryptocurrencies);
+    } on DioError catch (error) {
+      return Result.failure(error: error);
+    }
+  }
+
+  @override
+  Future<Result<Object, Exception>> fetchCryptocurrencyById({
+    required String id,
+    required bool marketData,
+    required bool sparkline,
+  }) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Result<MarketChartEntity, Exception>> fetchMarketChart({
+    required String id,
+    required String vsCurrency,
+    required String days,
+  }) async {
+    try {
+      final MarketChartResponse marketChartResponse =
+          await _cryptocurrenciesApi.fetchMarketChart(
+        id,
+        vsCurrency,
+        days,
+      );
+
+      final MarketChartEntity marketChartEntity =
+          marketChartResponse.toEntity();
+
+      return Result.success(data: marketChartEntity);
+    } on DioError catch (error) {
+      return Result.failure(error: error);
+    }
+  }
 }
